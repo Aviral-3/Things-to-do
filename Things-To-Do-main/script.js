4cd332b020cbf7ae0b68ca732756ae4cd3d37ca9
Aviral Srivastava <63746541+Aviral-3@users.noreply.github.com>
2021-06-30 19:42:43 +0530

    Add files via upload

---
+++ Things-To-Do-main/script.js
@@ -0,0 +1,28 @@
+// TODO:Getting DOM elements here
+
+const allActivityTypes = document.querySelectorAll(".item");
+const activityContent = document.querySelector(".activityContent");
+const participantsNumber = document.querySelector(".participants");
+
+// TODO: Processing here 
+
+let activityType = "education";
+let url = "https://www.boredapi.com/api/activity";
+function constructUrl(activity) {
+    return `${url}?type=${activity}`
+}
+allActivityTypes.forEach(element => {
+    element.addEventListener('click', () => {
+        activityType = (element.innerText).toLowerCase();
+        console.log(activityType);
+        fetch(constructUrl(activityType))
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                activityContent.innerHTML=`Your Task : ${data.activity}`;
+                participantsNumber.innerHTML =`Needed Participants : ${data.participants}`;
+            })
+    })
+});
+
+
